274fb8adc14fb4de1fe89fb9f97ae662d9f16ec8
CyrilYao <cyril.yao@oocl.com>
2019-07-18 16:45:16 +0800

    update prosecutor table

---
+++ src/main/resources/db/migration/V2__update_prosecutor.sql
@@ -0,0 +1,6 @@
+ALTER TABLE prosecutor ADD age INT;
+ALTER TABLE prosecutor ADD sex VARCHAR;
+ALTER TABLE prosecutor ADD birthday DATE;
+ALTER TABLE prosecutor ADD ID_number VARCHAR;
+ALTER TABLE prosecutor ADD experience INT;
+
